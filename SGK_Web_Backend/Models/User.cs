252644263b94d771fecb8e8871b0221662744703
Berikai <berkayeren2009@gmail.com>
2024-12-01 19:46:46 +0300

    Add User model

---
+++ SGK_Web_Backend/Models/User.cs
@@ -0,0 +1,13 @@
+﻿namespace SGK_Web_Backend.Models;
+
+public class User
+{
+    public Guid id { get; set; }
+    public string student_id { get; set; }
+    public string username { get; set; }
+    public string email { get; set; } // school email
+    public string password { get; set; } // hashed password with a salt: hash(password + salt)
+    public string name { get; set; }
+    public string surname { get; set; }
+    public string phone_number { get; set; }
+}
