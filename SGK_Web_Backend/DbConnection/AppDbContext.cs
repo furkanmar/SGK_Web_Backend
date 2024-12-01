377a8c7129a6e931ce4f451d51a305c78bf78cce
Berikai <berkayeren2009@gmail.com>
2024-12-01 19:45:51 +0300

    Add users table to AppDbContext

--- SGK_Web_Backend/DbConnection/AppDbContext.cs
+++ SGK_Web_Backend/DbConnection/AppDbContext.cs
@@ -8,5 +8,7 @@ public class AppDbContext: DbContext
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
     public DbSet<deneme> denemeler { get; set; }
+
+    public DbSet<User> users { get; set; }
     
 }
